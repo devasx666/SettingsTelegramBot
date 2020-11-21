55ac640f3360d77cf74b7e6d7e564d2a65eaa278
dev.asx666 <bezhuk.sv@gmail.com>
2020-11-22 00:46:43 +0200

    Add: Click for card list bot

--- app/src/main/java/com/rebus/settingstelegrambot/MainActivity.kt
+++ app/src/main/java/com/rebus/settingstelegrambot/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rebus.settingstelegrambot.data.db.roommodels.BotsData
 import com.rebus.settingstelegrambot.ui.adapters.BotsAdapter
+import com.rebus.settingstelegrambot.ui.event.OnClickListener
 import com.rebus.settingstelegrambot.ui.factory.BotViewModelFactory
 import com.rebus.settingstelegrambot.ui.viewmodel.BotsViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var botsViewModel: BotsViewModel
 
     private val botsArray: MutableList<BotsData> = mutableListOf()
@@ -26,15 +26,17 @@ class MainActivity : AppCompatActivity() {
         recyclerBotsInit()
         insertDB()
         getAllBots()
+    }
 
-//        openBotActions(TelegramBotActions())
+    override fun <T> onClickListener(item: T) {
+        openBotActions(TelegramBotActions())
     }
 
     private fun recyclerBotsInit() {
         bots_list.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = BotsAdapter(botsArray)
+            adapter = BotsAdapter(botsArray, this@MainActivity)
             (adapter as BotsAdapter).notifyDataSetChanged()
         }
     }
@@ -48,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         fab_add_bot.setOnClickListener {
             botsViewModel.addBot(
                 BotsData(
-                    1, "test", "test:token"
+                    6, "test", "test:token"
                 )
             )
         }
@@ -57,7 +59,7 @@ class MainActivity : AppCompatActivity() {
     private fun getAllBots() {
         botsViewModel.readAllData.observe(this, {
             setItemBotsCollections(it)
-
+            Log.d("TSET", it.toString())
             if (it.isNotEmpty()) {
                 bots_list.apply {
                     (adapter as BotsAdapter).notifyItemInserted(botsArray.last().id)
--- app/src/main/java/com/rebus/settingstelegrambot/ui/adapters/BotsAdapter.kt
+++ app/src/main/java/com/rebus/settingstelegrambot/ui/adapters/BotsAdapter.kt
@@ -5,14 +5,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.rebus.settingstelegrambot.data.db.roommodels.BotsData
 import com.rebus.settingstelegrambot.databinding.BotCardBinding
+import com.rebus.settingstelegrambot.ui.event.OnClickListener
 
-class BotsAdapter(private val bots: MutableList<BotsData>) : RecyclerView.Adapter<BotsAdapter.BotViewHolder>() {
+class BotsAdapter(
+    private val bots: MutableList<BotsData>,
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<BotsAdapter.BotViewHolder>() {
 
     override fun getItemCount(): Int =
         bots.size
 
     override fun onBindViewHolder(holder: BotViewHolder, position: Int) =
-        holder.bind(bots.get(position))
+        holder.bind(bots.get(position), onClickListener)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BotViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -22,8 +26,9 @@ class BotsAdapter(private val bots: MutableList<BotsData>) : RecyclerView.Adapte
     }
 
     class BotViewHolder(private val binding: BotCardBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: BotsData) {
+        fun bind(item: BotsData, listener: OnClickListener) {
             binding.bots = item
+            binding.itemClick = listener
             binding.executePendingBindings()
         }
     }
